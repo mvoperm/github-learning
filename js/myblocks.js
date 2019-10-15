7f5d5808d64f8e362ae627e21b0543c50f9f56fb
Maksim Oskotskiy <m.oskotskiy@mail.ru>
2019-10-15 18:12:16 +0300

    Первая версия блоков статей

--- js/myblocks.js
+++ js/myblocks.js
@@ -1,1 +1,17 @@
 console.log('Файл myblocks.js подключен!');
+
+Vue.component('content-item', {
+  props: ['itemblock'],
+  template: '<article>{{ itemblock.text }}</article>',
+});
+let article = new Vue({
+	el: '#items',
+	data: {
+		itemblocks: [
+			{ id: 0, text: 'Изучить JavaScript' },
+			{ id: 1, text: 'Изучить Vue.js' },
+			{ id: 2, text: 'Создать что-нибудь классное' },
+		],
+		clue: 'Элементы контента',
+	},
+});
