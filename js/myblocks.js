5697a3de594984c4c6ad75dd9d8eb9a17de97c0d
Maksim Oskotskiy <m.oskotskiy@mail.ru>
2019-10-16 22:30:41 +0300

    Получение данных в формате JSON и их отображение

--- js/myblocks.js
+++ js/myblocks.js
@@ -2,7 +2,8 @@ console.log('Файл myblocks.js подключен!');
 
 Vue.component('content-item', {
   props: ['itemblock'],
-  template: '<article><h3>{{ itemblock.title }}</h3><p>{{ itemblock.text }}</p><hr><br></article>',
+  //template: '<article><h3>{{ itemblock.title }}</h3><p>{{ itemblock.text }}</p><hr><br></article>',
+  template: '<p>{{ itemblock.text }}</p>',
 });
 let article = new Vue({
 	el: '#items',
@@ -12,8 +13,19 @@ let article = new Vue({
 			{ id: 1, title: 'Элемент 1', text: 'Изучить Vue.js' },
 			{ id: 2, title: 'Элемент 2', text: 'Создать что-нибудь классное' },
 		],
+    cssproperties: [],
 		clue: 'Элементы контента',
 	},
 });
 
 article.itemblocks.push({ id: 3, title: 'Элемент 3', text: 'Profit!' });
+
+(async () =>	{
+  const cssRulesList = ['basic_hue', 'article_transparency', 'bg_image', 'basic_font_type', 'basic_font_size',];
+  let jsonFile = await fetch('../data/data.json');
+  let jsonObj = await jsonFile.json();
+  cssRulesList.forEach((item, index) => {
+    article.cssproperties.push({ id: index, text: jsonObj[item] });
+  });
+  console.log(JSON.stringify(jsonObj));
+})();
