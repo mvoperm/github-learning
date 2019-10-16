deca4dd4a22b5f6b9c73ecb15a1ab73d2479385b
Maksim Oskotskiy <m.oskotskiy@mail.ru>
2019-10-16 14:01:35 +0300

    Усложнение компонента content-item

--- js/myblocks.js
+++ js/myblocks.js
@@ -2,16 +2,18 @@ console.log('Файл myblocks.js подключен!');
 
 Vue.component('content-item', {
   props: ['itemblock'],
-  template: '<article>{{ itemblock.text }}</article>',
+  template: '<article><h3>{{ itemblock.title }}</h3><p>{{ itemblock.text }}</p><hr><br></article>',
 });
 let article = new Vue({
 	el: '#items',
 	data: {
 		itemblocks: [
-			{ id: 0, text: 'Изучить JavaScript' },
-			{ id: 1, text: 'Изучить Vue.js' },
-			{ id: 2, text: 'Создать что-нибудь классное' },
+			{ id: 0, title: 'Элемент 0', text: 'Изучить JavaScript' },
+			{ id: 1, title: 'Элемент 1', text: 'Изучить Vue.js' },
+			{ id: 2, title: 'Элемент 2', text: 'Создать что-нибудь классное' },
 		],
 		clue: 'Элементы контента',
 	},
 });
+
+article.itemblocks.push({ id: 3, title: 'Элемент 3', text: 'Profit!' });
